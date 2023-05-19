e0c6522c0f05b57a5a2a9392e730d82746ed9b08
toperrrrrrrr <90093314+toperrrrrrrr@users.noreply.github.com>
2023-05-19 15:31:30 +0800

    Start from scratch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+const App = () => {
+
+  const [counter, setCounter] = useState(0);
+
+  const isNameShowing = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <button onClick={()=> setCounter(prevCount=> prevCount -1)}>-</button>
+    <h1>{counter}</h1>
+    <button onClick={()=> setCounter(prevCount=> prevCount +1)}>+</button>
+
     </div>
   );
 }

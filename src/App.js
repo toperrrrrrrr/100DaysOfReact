0c59cc41a1da49f21bb1ca0f839b5c00fb1e141a
toperrrrrrrr <90093314+toperrrrrrrr@users.noreply.github.com>
2023-05-22 09:07:38 +0800

    Stripped down to the basic lines of codes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React from "react";
-import {useEffect} from "react";
 
-//76ddda82
-
- const API_Key = 'http://www.omdbapi.com/?apikey=76ddda82'; 
 
 const App = () => {
-
-    const searchitems = async (title) => {
-        const response = await fetch(`${API_Key}&s=${title}`);
-                const data = await response.json();
-                console.log(data);
-                return data;
-
-                console.log(data.Search);
-    }
-
-    useEffect   (   ()  => {   searchitems('spiderman')      }, []);
 
     return (
             <div>

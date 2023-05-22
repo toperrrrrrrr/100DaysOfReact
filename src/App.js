8d0fb4ba3419541a94ed43f0f83726df61af5bc8
toperrrrrrrr <90093314+toperrrrrrrr@users.noreply.github.com>
2023-05-22 10:08:57 +0800

    some errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 
-
 const App = () => {
+    var inputs = document.getElementById('inputs');
+
+    
+    function toSentenceCase(text) {
+        return text.toLowerCase().replace(/(^|\.)\s*([a-z])/g, function(match, p1, p2) {
+          return p1 + p2.toUpperCase();
+        });
+      }
+    
+
+      const buss = ()=>  {
+        var convertedText = toSentenceCase(inputs.value);
+        inputs.value = convertedText;   
+      };
+      
 
     return (
             <div>
-                <h1>Hello World</h1>
+                <h3>Case Converter</h3> <br/>
+                <textarea id="inputs" placeholder="Enter text to convert"></textarea>
+                <button type="button" onClick={buss}>Sentence case</button>
             </div>
         );
 

268988ea3b3426a810fb046cc23788a8e63b0d39
toperrrrrrrr <90093314+toperrrrrrrr@users.noreply.github.com>
2023-05-30 20:20:33 +0800

    simple login page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from 'react';
+import './Login.css';
 
-const App = () => {
- 
-    const [name, setName] = useState("");
-  
-    return (
-      <>
-      <form>
-        <label>Enter your name:
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login validation here
+    if (username === 'admin' && password === 'password') {
+      setLoggedIn(true);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      {isLoggedIn ? (
+        <div className="welcome-message">Welcome, {username}!</div>
+      ) : (
+        <form className="login-form" onSubmit={handleSubmit}>
           <input
-            type="text" 
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleUsernameChange}
           />
-        </label>
-      </form>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button type="submit">Login</button>
+        </form>
+      )}
+    </div>
+  );
+};
 
-      <h1>{name}</h1>
-      </>
-    )
-}
-
-
-export default App;
+export default Login;
